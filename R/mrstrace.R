7185ab7c14ff8d9c7cf66adeb5d732526cde3ba7
michaelschwob <michaelr@schwob.cc>
2025-01-06 13:22:40 -0600

    add mean to trace plotting function

--- R/mrstrace.R
+++ R/mrstrace.R
@@ -1,4 +1,4 @@
-mrs.trace <- function(chain, par_name, thin = 1, burn = floor(length(chain)*0.1)){
+mrs.trace <- function(chain, par_name, thin = 1, burn = floor(length(chain)*0.1), mean = TRUE){
   new.chain <- chain[seq(burn, length(chain), by=thin)]
 
   if(missing(par_name)){
@@ -12,5 +12,8 @@ mrs.trace <- function(chain, par_name, thin = 1, burn = floor(length(chain)*0.1)
   dfmrs.trace <- data.frame(cbind(new.chain = new.chain, iter = 1:length(new.chain)))
 
   plot <- ggplot(dfmrs.trace, aes(x = iter, y = new.chain)) + theme_classic() + geom_line() + xlab("Iterations") + ylab(par) + ggtitle(title) + theme(plot.title = element_text(hjust = 0.5))
+  if(mean == TRUE){
+    plot = plot + geom_hline(yintercept = mean(chain), col = "red")
+  }
   return(plot)
 }
